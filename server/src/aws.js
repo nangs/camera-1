5e2434f6af3d5a8af2607541d56a3bf42aa48093
Toan <dinhtoan.vn@gmail.com>
2018-03-27 10:31:39 +0700

    Update aws.js

--- server/src/aws.js
+++ server/src/aws.js
@@ -1,7 +1,7 @@
 export const s3Config = {
     bucket: 'camera.tabvn.com',
-    accessKeyId: 'AKIAIJ7XE6IDO6V6RZUQ',
-    secretAccessKey: "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",
+    accessKeyId: '',
+    secretAccessKey: "",
     region: "us-east-1"
 };
 
